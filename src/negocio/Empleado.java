f7c1e50eb88d8ac78eebfce151fae2f709449ec3
Alexis Castaño <ale.ily@hotmail.com>
2018-09-24 18:12:33 -0300

    Se crea clase Empleado a modo de prueba

---
+++ src/negocio/Empleado.java
@@ -0,0 +1,18 @@
+package negocio;
+
+public class Empleado {
+	private String nombre;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	//Esto es una prueba para subir algo de codigo, habria que hacer las propiedades 
+	//posta del Empleado con sus get y set
+	
+
+}
