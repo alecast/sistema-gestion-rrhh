3472561bf4e7596f80f8a1e8e97be4db37cba5fb
Alexis <a.castano.landin@gmail.com>
2018-10-31 15:58:43 -0300

    Revert "vinculamos con la base de datos
    This reverts commit ccf17e1da0258b39ad2d966f6a3e046ad7a5a93d.

--- src/datos/DBConnection.java
+++ src/datos/DBConnection.java
@@ -7,21 +7,11 @@ public class DBConnection {
 	
 	public static Connection createConnection() {
 		//Datos de conexión a la base (ver cómo generalizar)
-		//String conString = "jdbc:mysql://localhost:3306/gestionrrhh?serverTimezone=UTC";
-		try {
-            // The newInstance() call is a work around for some
-            // broken Java implementations
-
-            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
-        } catch (Exception ex) {
-            // handle the error
-        }
-		String conString = "jdbc:mysql://localhost:3306/javapro?serverTimezone=UTC";
+		String conString = "jdbc:mysql://localhost:3306/gestionrrhh?serverTimezone=UTC";
 		String user = "root";
 		String pass = "root";
 		Connection con = null;
 		try {
-			
 			con = DriverManager.getConnection(conString,user,pass); //Conecta a la DB
 		} catch (SQLException e) {
 			e.printStackTrace();
